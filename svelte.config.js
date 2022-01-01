5c1b26672549579b5a7ba72d1a83e22c078232d1
HiiD <salomone.marco@outlook.it>
2022-01-01 22:27:04 +0100

    Added micromatch to avoid putting stories inside the package

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import mm from 'micromatch';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,6 +8,11 @@ const config = {
     kit: {
         adapter: adapter(),
         target: '#svelte',
+        package: {
+            exports: (filepath) => mm.isMatch(filepath, ['!**/_*', '!**/internal/**']),
+            //Exclude stories
+            files: mm.matcher('!**/*.stories.svelte')
+        }
     },
 };
 
